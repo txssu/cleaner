7371ed1120e6f17ea21f6162eb450f45107d3800
Eugene <git@telegrafo.ru>
2024-03-19 19:28:11 +0500

    Delete all non-wining dices

--- lib/cleaner/bot.ex
+++ lib/cleaner/bot.ex
@@ -2,15 +2,30 @@ defmodule Cleaner.Bot do
   @moduledoc false
   use ExGram.Bot, name: __MODULE__, setup_commands: true
 
-  command("start")
+  command("ping")
 
   middleware(ExGram.Middleware.IgnoreUsername)
 
-  def handle({:command, :start, _msg}, context) do
-    answer(context, "Даров!")
+  def handle({:command, :ping, _msg}, context) do
+    answer(context, "pong")
   end
 
-  def handle({:text, text, _msg}, context) do
-    answer(context, text)
+  def handle({:message, %{dice: %{emoji: "🎰", value: value}} = message}, context) do
+    if winning_dice?(value) do
+      context
+    else
+      delete(context, message)
+    end
+  end
+
+  def handle(_event, context), do: context
+
+  defp winning_dice?(dice_value) do
+    <<right::binary-size(2), center::binary-size(2), left::binary-size(2)>> =
+      (dice_value - 1)
+      |> Integer.to_string(2)
+      |> String.pad_leading(6, "0")
+
+    left == center and center == right
   end
 end
