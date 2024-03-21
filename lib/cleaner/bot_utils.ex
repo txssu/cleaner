12062b05cadaaa2eacdc328e88ea181f68beba64
Eugene <git@telegrafo.ru>
2024-03-21 22:29:38 +0500

    Add rate limitter on sending messages

--- lib/cleaner/bot_utils.ex
+++ lib/cleaner/bot_utils.ex
@@ -7,12 +7,22 @@ defmodule Cleaner.BotUtils do
     send_options = Keyword.put(options, :bot, Cleaner.Bot)
     chat_id = context.update.message.chat.id
 
-    with {:ok, message} = ExGram.send_message(chat_id, text, send_options) do
+    with {:ok, message} = maybe_send_mesage(chat_id, text, send_options) do
       delay = context.extra.chat_config.delete_delay_in_seconds
       DelayMessageRemover.schedule_delete_message(chat_id, message.message_id, delay)
       DelayMessageRemover.schedule_delete_message(chat_id, context.update.message.message_id, delay)
 
       :ok
+    end
+  end
+
+  def maybe_send_mesage(chat_id, text, send_options) do
+    case Hammer.check_rate("send_message:#{chat_id}", 60_000, 20) do
+      {:allow, _count} ->
+        ExGram.send_message(chat_id, text, send_options)
+
+      {:deny, _limit} ->
+        {:error, :rate_limit}
     end
   end
 end
--- mix.exs
+++ mix.exs
@@ -30,6 +30,7 @@ defmodule Cleaner.MixProject do
       {:ecto_sql, "~> 3.0"},
       {:postgrex, ">= 0.0.0"},
       {:ecto_boot_migration, "~> 0.3.0"},
+      {:hammer, "~> 6.1"},
       {:credo, "~> 1.7.3", only: [:dev, :test], runtime: false},
       {:dialyxir, "~> 1.4.3", only: [:dev, :test], runtime: false},
       {:mix_audit, "~> 2.1.2", only: [:dev, :test], runtime: false},
