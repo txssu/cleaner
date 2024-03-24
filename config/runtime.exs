77414b38366f8456de759656a42ef3792e30b923
Eugene <git@telegrafo.ru>
2024-03-24 11:59:44 +0500

    Remove env dependency

--- config/runtime.exs
+++ config/runtime.exs
@@ -1,6 +1,6 @@
 import Config
 
-config :cleaner, Cleaner.Bot, telegram_token: System.fetch_env!("TELEGRAM_TOKEN")
+config :cleaner, Cleaner.Bot, telegram_token: System.get_env("TELEGRAM_TOKEN")
 
 if config_env() == :prod do
   config :cleaner, Cleaner.Repo,
