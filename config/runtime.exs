b35b757203a83fbb11370759e5c4160122b4e2bd
Eugene <git@telegrafo.ru>
2024-03-26 20:00:21 +0500

    Update docker compose starting

--- config/runtime.exs
+++ config/runtime.exs
@@ -4,6 +4,6 @@ config :cleaner, Cleaner.Bot, telegram_token: System.get_env("TELEGRAM_TOKEN")
 
 if config_env() == :prod do
   config :cleaner, Cleaner.Repo,
-    url: "ecto://postgres:postgres@database/cleaner_prod",
+    url: System.get_env("DATABASE_URL", "ecto://postgres:postgres@database/cleaner_prod"),
     pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10")
 end
