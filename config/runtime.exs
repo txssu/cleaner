6127003a78bc26074e778a2ac8b5b0946e1a2b01
Eugene <git@telegrafo.ru>
2024-05-05 20:02:07 +0500

    Fix config

--- config/runtime.exs
+++ config/runtime.exs
@@ -6,8 +6,8 @@ if config_env() == :prod do
   config :cleaner, Cleaner.Repo,
     url: System.get_env("DATABASE_URL", "ecto://postgres:postgres@database/cleaner_prod"),
     pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10")
-end
 
-config :cleaner, Cleaner.AI.OpenAIClient,
-  api_url: System.fetch_env!("OPENAI_URL"),
-  api_key: System.fetch_env!("OPENAI_KEY")
+  config :cleaner, Cleaner.AI.OpenAIClient,
+    api_url: System.get_env("OPENAI_URL", "https://api.openai.com/v1"),
+    api_key: System.fetch_env!("OPENAI_KEY")
+end
