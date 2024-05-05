a30cd2f1cb937507a3f780d6ca0505db1a8d58aa
Eugene <git@telegrafo.ru>
2024-05-05 20:24:58 +0500

    Explicitly set tesla adapter

--- config/config.exs
+++ config/config.exs
@@ -5,6 +5,8 @@ config :cleaner, ecto_repos: [Cleaner.Repo]
 config :hammer,
   backend: {Hammer.Backend.ETS, [expiry_ms: 60_000 * 60 * 4, cleanup_interval_ms: 60_000 * 10]}
 
+config :tesla, adapter: Tesla.Adapter.Hackney
+
 env_config = "#{config_env()}.exs"
 
 if "config" |> Path.join(env_config) |> File.exists?() do
--- config/dev.exs
+++ config/dev.exs
@@ -7,3 +7,7 @@ config :cleaner, Cleaner.Repo,
   database: "cleaner_dev",
   show_sensitive_data_on_connection_error: true,
   pool_size: 10
+
+config :cleaner, Cleaner.AI.OpenAIClient,
+  api_url: System.fetch_env!("OPENAI_URL"),
+  api_key: System.fetch_env!("OPENAI_KEY")
