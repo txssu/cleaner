78ed2d9e125a3724eb61a24d3ed258c661177ff3
Eugene <git@telegrafo.ru>
2024-05-07 01:37:49 +0500

    Increase OpenAI API timeout

--- lib/cleaner/ai/open_ai_client.ex
+++ lib/cleaner/ai/open_ai_client.ex
@@ -7,6 +7,7 @@ defmodule Cleaner.AI.OpenAIClient do
   plug(Tesla.Middleware.BaseUrl, api_url())
   plug(Tesla.Middleware.Headers, [{"authorization", api_key()}])
   plug(Tesla.Middleware.JSON)
+  plug(Tesla.Middleware.Timeout, timeout: 10_000)
 
   @type message :: %{role: String.t(), content: String.t()}
 
