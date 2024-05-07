d5ef893775c64e0c4021c49445f9c89c78618a1f
Eugene <git@telegrafo.ru>
2024-05-07 23:00:49 +0500

    Add timeout to tesla adapter

--- config/config.exs
+++ config/config.exs
@@ -5,7 +5,7 @@ config :cleaner, ecto_repos: [Cleaner.Repo]
 config :hammer,
   backend: {Hammer.Backend.ETS, [expiry_ms: 60_000 * 60 * 4, cleanup_interval_ms: 60_000 * 10]}
 
-config :tesla, adapter: Tesla.Adapter.Hackney
+config :tesla, adapter: {Tesla.Adapter.Hackney, recv_timeout: 30_000}
 
 env_config = "#{config_env()}.exs"
 
