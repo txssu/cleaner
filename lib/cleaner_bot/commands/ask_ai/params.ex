9a2e08ef5a075993f2c84cc0a0dda2815d8be98b
txssu <git@telegrafo.ru>
2024-12-03 23:42:30 +0500

    Add extraction text from caption

--- lib/cleaner_bot/commands/ask_ai/params.ex
+++ lib/cleaner_bot/commands/ask_ai/params.ex
@@ -1,5 +1,7 @@
 defmodule CleanerBot.Commands.AskAI.Params do
   @moduledoc false
+  alias ExGram.Model.Message
+  alias ExGram.Model.TextQuote
   alias ExGram.Model.User
 
   defstruct ~w[user text prompt admin? internal_user reply_to chat_id]a
@@ -30,7 +32,8 @@ defmodule CleanerBot.Commands.AskAI.Params do
   def from_context(context, message) do
     %{extra: %{admin?: admin?, chat_config: %{ai_prompt: prompt}, internal_user: internal_user}} = context
 
-    %{chat: %{id: chat_id}, text: text, reply_to_message: reply_to, quote: quote_data, from: user} = message
+    %{chat: %{id: chat_id}, reply_to_message: reply_to, quote: quote_data, from: user} = message
+    text = extract_text(message)
 
     reply_to_bot? = reply_to && reply_to.from.id == context.bot_info.id
 
@@ -45,21 +48,31 @@ defmodule CleanerBot.Commands.AskAI.Params do
     }
   end
 
-  defp transform_reply_to(message, reply_message, reply_to_bot?)
+  defp transform_reply_to(reply_message, quote_data, reply_to_bot?)
 
-  defp transform_reply_to(nil, _quote_data, _reply_to_bot?) do
+  defp transform_reply_to(%Message{text: message_text} = message, nil, reply_to_bot?) when is_binary(message_text) do
+    text = extract_text(message)
+
+    %ReplyTo{
+      message_id: message.message_id,
+      text: if(reply_to_bot?, do: nil, else: text),
+      from_username: if(reply_to_bot?, do: nil, else: message.from.first_name)
+    }
+  end
+
+  defp transform_reply_to(%Message{} = message, %TextQuote{} = quote_data, reply_to_bot?) do
+    %ReplyTo{
+      message_id: message.message_id,
+      text: quote_data.text,
+      from_username: if(reply_to_bot?, do: nil, else: message.from.first_name)
+    }
+  end
+
+  defp transform_reply_to(_reply_message, _quote_data, _reply_to_bot?) do
     %ReplyTo{message_id: nil, text: nil}
   end
 
-  defp transform_reply_to(%{message_id: message_id, from: %{first_name: username}}, %{text: text}, reply_to_bot?) do
-    %ReplyTo{message_id: message_id, text: text, from_username: if(reply_to_bot?, do: nil, else: username)}
-  end
-
-  defp transform_reply_to(%{message_id: message_id, text: text, from: %{first_name: username}}, nil, reply_to_bot?) do
-    %ReplyTo{
-      message_id: message_id,
-      text: if(reply_to_bot?, do: nil, else: text),
-      from_username: if(reply_to_bot?, do: nil, else: username)
-    }
+  defp extract_text(%Message{caption: caption, text: text}) do
+    text || caption
   end
 end
